90799b8015af718492f8c1c3d957ffcf78d87ea8
Prakash Pandey <prakash@caringcompany.in>
2019-01-21 20:21:25 +0530

    Inplemented database interface in mongodb.go

--- database/database.go
+++
@@ -1,7 +0,0 @@
-package database
-
-// Database interface contains all those methods required for performing database operations.
-// All database should implement this interface.
-type Database interface {
-	InsertUser() error
-}
